a0b4c4f587c977570030ade138458c6aa1896e4d
Compozedani03 <compozedani03@gmail.com>
2016-04-21 17:45:38 +0100

    added full class functionality for the validator class

--- exercise0.js
+++ exercise0.js
@@ -1,14 +1,129 @@
 'use strict'
 
-class validator{
+class validator {
 
-  validateThis(inputString){
-    var validString = false
-    if(inputString === 'A Valid String'){
-      validString = true
-    }
-    return validString
+  constructor(valCriteria) {
+      this._valCriteria = valCriteria
+      this._validationMessage = []
   }
+
+  get validationMessage(){
+    return this._validationMessage
+  }
+
+  validateThis(inputObject){
+    var validObject = false
+
+
+
+
+    if(typeof inputObject === 'undefined'){
+      throw 'Invalid object passed'
+    }
+    if (Object.keys(inputObject).length===0) {
+      throw 'Empty object passed'
+    }
+
+    // putthis in reduce
+    var invalidObj = false
+    for (var key in inputObject) {
+
+      if (this._valCriteria.hasOwnProperty(key)) {
+
+        var dataTypes = Object.keys(this._valCriteria[key]);
+
+          for(var dataType in dataTypes){
+
+              switch(dataTypes[dataType]){
+                case 'type':
+                  if(typeof inputObject[key] !== this._valCriteria[key].type) {
+                  invalidObj= true
+                  this._validationMessage.push('The '+ key + ' is not a ' + this._valCriteria[key].type)
+                  }
+                  break;
+
+                case 'length':
+                  if (this._valCriteria[key].length.hasOwnProperty('greaterThan')){
+
+                    if (inputObject[key] <= this._valCriteria[key].length.greaterThan){
+                      invalidObj= true
+                      this._validationMessage.push('The ' + key + ' is Too Short')
+                    }
+                  }
+                  break;
+              }
+            }
+          }
+        }
+
+
+        // var isvalid = Object.keys(inputObject).reduce(applyValidation, true)
+        // console.log('Result of Array Reduce = ' + isvalid)
+        //
+        // function applyValidation(result, element) {
+        //
+        //   if (inputObject.hasOwnProperty(element)) {
+        //
+        //     var dataTypes = Object.keys(inputObject[element]);
+        //
+        //       for(var dataType in dataTypes){
+        //
+        //           switch(dataTypes[dataType]){
+        //             case 'type':
+        //               if(typeof inputObject[element] !== inputObject[element].type) {
+        //               invalidObj= true
+        //               this._validationMessage.push('The '+ element + ' is not a ' + inputObject[element].type)
+        //               }
+        //               break;
+        //
+        //             case 'length':
+        //               if (inputObject[element].length.hasOwnProperty('greaterThan')){
+        //
+        //                 if (inputObject[element] <= inputObject[element].length.greaterThan){
+        //                   invalidObj= true
+        //                   this._validationMessage.push('The ' + element + ' is Too Short')
+        //                 }
+        //               }
+        //               break;
+        //           }
+        //         }
+        //       }
+        //
+        //   return true;
+        //
+        // }
+
+        // if (this._valCriteria[key].hasOwnProperty('type')) {
+        //
+        //      if(typeof inputObject[key] !== this._valCriteria[key].type) {
+        //      invalidObj= true
+        //      this._validationMessage += 'The '+ key + ' is not a ' + this._valCriteria[key].type
+        //   }
+        // }
+        //
+        // if (this._valCriteria[key].hasOwnProperty('length')) {
+        //
+        //    if (this._valCriteria[key].length.hasOwnProperty('greaterThan'))
+        //    {
+        //      if (inputObject[key] <= this._valCriteria[key].length.greaterThan){
+        //        invalidObj= true
+        //        this._validationMessage += 'The ' + key + ' is Too Short'
+        //      }
+        //
+        //    }
+        //  }
+
+    // if(typeof inputObject.make === this._valCriteria.make.type && inputObject.make.length > this._valCriteria.make.length.greaterThan &&
+    //     typeof inputObject.model === this._valCriteria.model.type && inputObject.model.length > this._valCriteria.model.length.greaterThan &&
+    //       typeof inputObject.year === this._valCriteria.year.type){
+    //   validObject = true
+    // }else{
+    //   this._validationMessage = 'The Make is Too Short'
+    // }
+    return !invalidObj
+  }
+
+
 }
 
 module.exports = validator
--- test/exercise0_test.js
+++ test/exercise0_test.js
@@ -6,20 +6,110 @@ var validator = require('../exercise0')
 
 describe('Given a validator', function() {
 
+  var carValidatations = {
+    make: {type: 'string', length: { greaterThan: 0 } },
+    model: {type: 'string', length: { greaterThan: 0 } },
+    year: {type: 'number'}
+    }
+
+  var carValidator
+
+  beforeEach( function () {
+    carValidator = new validator(carValidatations)
+  })
+
+  describe('when no data is provided', function () {
+    it('then the validator throws an error', function () {
+      function invalidObjectPassed(){
+        carValidator.validateThis()
+      }
+      expect(invalidObjectPassed).to.throw(/Invalid object passed/)
+    })
+  })
+
+  describe('when empty data is provided', function () {
+    it('then the validator throws an error', function () {
+      function emptyObjectPassed(){
+        carValidator.validateThis({})
+      }
+      expect(emptyObjectPassed).to.throw(/Empty object passed/)
+    })
+  })
+
   describe('when a set of valid data is provided', function() {
 
+    var validCar = {
+      make: 'Honda',
+      model: 'Accord',
+      year: 1964
+    }
+
     it('then the validator returns true', function () {
-      var validatorObj = new validator()
-      expect(validatorObj.validateThis('A Valid String')).to.be.true
+
+      expect(carValidator.validateThis(validCar)).to.be.true
     })
 
   })
 
   describe('when a set of invalid data is provided', function () {
 
+    var invalidCar = {
+      make: '',
+      model: 'Accord',
+      year: 1964
+    }
     it('then the validator returns false', function() {
-      var validateObj = new validator()
-      expect(validateObj.validateThis('This is not a valid string')).to.be.false
+      expect(carValidator.validateThis(invalidCar)).to.be.false
     })
+
+    it('the validator returns a message stating the make is too short', function () {
+      carValidator.validateThis(invalidCar)
+      expect(carValidator.validationMessage[0]).to.equal('The make is Too Short')
+    })
+
+    var invalidCar2 = {
+      make: 'Honda',
+      model: '',
+      year: 1964
+    }
+    it('the validator returns a message stating the model is too short', function () {
+      carValidator.validateThis(invalidCar2)
+      expect(carValidator.validationMessage[0]).to.equal('The model is Too Short')
+    })
+
+    var invalidCar3 = {
+      make: 'Honda',
+      model: 'Accord',
+      year: '1964'
+    }
+    it('the validator returns a message stating that the year is not a number', function () {
+      carValidator.validateThis(invalidCar3)
+      expect(carValidator.validationMessage[0]).to.equal('The year is not a number')
+    })
+
+    var invalidCar4 = {
+      make: 233564,
+      model: 'Accord',
+      year: 1964
+    }
+    it('the validator returns a message stating that the make is not a string', function () {
+      carValidator.validateThis(invalidCar4)
+      expect(carValidator.validationMessage[0]).to.equal('The make is not a string')
+    })
+
+    var invalidCar5 = {
+      make: 233564,
+      model: '',
+      year: '1964'
+    }
+    it('the validator returns 3 messages', function () {
+      carValidator.validateThis(invalidCar5)
+      var messages = carValidator.validationMessage
+      expect(messages.length).to.equal(3)
+      expect(messages[0]).to.equal('The make is not a string')
+      expect(messages[1]).to.equal('The model is Too Short')
+      expect(messages[2]).to.equal('The year is not a number')
+    })
+
   })
 })
